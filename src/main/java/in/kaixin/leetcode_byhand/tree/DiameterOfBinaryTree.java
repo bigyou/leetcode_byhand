7b1dddce69ad4ccc6dbf64a3e691391ae5f40f05
bigyou <903088658@qq.com>
2022-02-25 18:00:45 +0800

    f

--- src/main/java/in/kaixin/leetcode_byhand/tree/DiameterOfBinaryTree.java
+++ src/main/java/in/kaixin/leetcode_byhand/tree/DiameterOfBinaryTree.java
@@ -1,9 +1,46 @@
 package in.kaixin.leetcode_byhand.tree;
 
 import in.kaixin.leetcode_byhand.common.TreeNode;
+import in.kaixin.leetcode_byhand.utils.Utils;
+
+import java.util.LinkedList;
+import java.util.List;
 
 public class DiameterOfBinaryTree {
     public int diameterOfBinaryTree(TreeNode root) {
+        List<TreeNode> nodeList = new LinkedList<>();
+        nodeList.add(root);
+        int res = Integer.MIN_VALUE;
+        int i = 0;
+        while (i < nodeList.size()) {
+            TreeNode node = nodeList.get(i);
+            int max = maxPath(node.left) + maxPath(node.right);
+            res = Math.max(max, res);
+            if (max > res / 2) {//
+                if (node.left != null) {
+                    nodeList.add(node.left);
+                }
+                if (node.right != null) {
+                    nodeList.add(node.right);
+                }
+            }
+            i++;
+        }
 
+
+        return res;
+    }
+
+    public int maxPath(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(maxPath(node.left) + 1, maxPath(node.right) + 1);
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode = Utils.getTreeNode(new Integer[]{4, -7, -3, null, null, -9, -3, 9, -7, -4, null, 6, null, -6, -6, null, null, 0, 6, 5, null, 9, null, null, -1, -4, null, null, null, -2});
+        DiameterOfBinaryTree diameterOfBinaryTree = new DiameterOfBinaryTree();
+        System.out.println(diameterOfBinaryTree.diameterOfBinaryTree(treeNode));
     }
 }
