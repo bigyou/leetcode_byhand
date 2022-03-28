1f8b51379a865171111998e908e0d7c64fafdcf0
bigyou <903088658@qq.com>
2022-03-28 15:33:03 +0800

    s

--- src/main/java/in/kaixin/leetcode_byhand/numbers/FindKthNumber.java
+++ src/main/java/in/kaixin/leetcode_byhand/numbers/FindKthNumber.java
@@ -1,10 +1,24 @@
 //package in.kaixin.leetcode_byhand.numbers;
 //
+//import java.util.Stack;
+//
 //public class FindKthNumber {
-////    https://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order/
-//    public int findKthNumber(int n, int k) {
+//    //    https://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order/
+//    public int findKthNumber(int n, int k) { TODO
+////        1 ,10,100,101,102,10311,12,13,14,15,16,17,18,19,2,3
+////        1,10,100,100,1
+//        String nstr = ("" + n);
+//        int length = nstr.length();
+//        int i = 1;
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(1);
+//        int v = 1;
+//        while (i < k) {
 //
 //
+//        }
 //
+//
+//        return 0;
 //    }
 //}
