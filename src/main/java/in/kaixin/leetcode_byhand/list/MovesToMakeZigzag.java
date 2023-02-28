3c883e7834d82d7350c22b8168a347b1ad4400e3
bigyou <903088658@qq.com>
2023-02-28 12:35:20 +0800

    add

--- src/main/java/in/kaixin/leetcode_byhand/list/MovesToMakeZigzag.java
+++ src/main/java/in/kaixin/leetcode_byhand/list/MovesToMakeZigzag.java
@@ -3,10 +3,28 @@ package in.kaixin.leetcode_byhand.list;
 public class MovesToMakeZigzag {
     //    https://leetcode.cn/problems/decrease-elements-to-make-array-zigzag/
     public int movesToMakeZigzag(int[] nums) {
-
-        int ret = Math.min(moves(copy(nums), 0), moves(copy(nums), 1));
+        int ret = Math.min(moves(nums, 0), moves(nums, 1));
         return ret;
     }
+
+
+    public int moves(int[] nums, int start) {
+        int times = 0;
+        for (int i = start; i < nums.length; i += 2) {
+            int step = 0;
+            if (i - 1 >= 0) {
+                step = Math.max(step, nums[i] - nums[i - 1] + 1);
+            }
+            if (i + 1 < nums.length) {
+                step = Math.max(step, nums[i] - nums[i + 1] + 1);
+            }
+
+            times += step;
+
+        }
+        return times;
+    }
+
 
     public int[] copy(int[] nums) {
         int[] cp = new int[nums.length];
@@ -16,7 +34,7 @@ public class MovesToMakeZigzag {
         return cp;
     }
 
-    public int moves(int[] numes, int start) {
+    public int moves2(int[] numes, int start) {
         int times = 0;
         for (int i = start; i < numes.length; ) {
             if (i - 1 >= 0) {
@@ -37,6 +55,7 @@ public class MovesToMakeZigzag {
         System.out.println(times);
         return times;
     }
+
 
     public static void main(String... args) {
         int[] nums = new int[]{7, 4, 8, 9, 7, 7, 5};
