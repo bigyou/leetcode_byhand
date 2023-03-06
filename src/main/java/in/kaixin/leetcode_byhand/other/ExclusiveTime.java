fd9093904cde373e723ef9eb44dbe8e98ae2b171
bigyou <903088658@qq.com>
2023-03-06 09:12:51 +0800

    not finihed

---
+++ src/main/java/in/kaixin/leetcode_byhand/other/ExclusiveTime.java
@@ -0,0 +1,139 @@
+package in.kaixin.leetcode_byhand.other;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.*;
+
+public class ExclusiveTime {
+    //    https://leetcode.cn/problems/exclusive-time-of-functions/description/
+    //    public int[] exclusiveTime(int n, List<String> logs) {
+//        int[] processArr = new int[n];
+//        for (int i = 0; i < processArr.length; i++) {
+//            processArr[i] = -1;
+//        }
+//        int[] ret = new int[n];
+//        for (int i = 0; i < logs.size(); i++) {
+//            String log = logs.get(i);
+//            String[] info = log.split(":");
+//            int threadNo = Integer.parseInt(info[0]);
+//            String process = info[1];
+//            int time = Integer.parseInt(info[2]);
+//            if ("start".equals(process)) {
+//                processArr[threadNo] = time;
+//            } else if ("end".equals(process)) {
+//                int cost = time - processArr[threadNo] + 1;
+//                processArr[threadNo] = -1;
+//                ret[threadNo] += cost;
+//            }
+//
+//        }
+//        return ret;
+//
+//    }
+//    public int[] exclusiveTime(int n, List<String> logs) {
+//        int[] ret = new int[n];
+//        int preStart = 0;
+//        int preThreadNo = -1;
+//        for (String str : logs) {
+//            String[] info = str.split(":");
+//            int threadNo = Integer.parseInt(info[0]);
+//            String process = info[1];
+//            int time = Integer.parseInt(info[2]);
+//            if ("start".equals(process)) {
+//                if (preThreadNo != -1) {//第一条记录
+//                    int cost = time - preStart;
+//                    ret[preThreadNo] += cost;
+//                }
+//                preThreadNo = threadNo;
+//                preStart = time;
+//            } else if ("end".equals(process)) {
+//                int cost = time - preStart + 1;
+//                ret[threadNo] += cost;
+//                preStart = time + 1;
+//                preThreadNo = -1;
+//            }
+//        }
+//        return ret;
+//    }
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int[] res = new int[n];
+        Stack<PrcessInfo> stack = new Stack<>();
+        long currentT = -1;
+        for (String str : logs) {
+            PrcessInfo info = parse(str);
+            if ("start".equals(info.getProcessStatus())) {
+                stack.push(info);
+                currentT = info.getT();
+            } else if ("end".equals(info.getProcessStatus())) {
+                PrcessInfo peek = stack.peek();
+                if (peek.getTrdNo() == info.getTrdNo()) {
+                    PrcessInfo pop = stack.pop();
+                    res[peek.getTrdNo()] += info.getT() - pop.getT() + 1;
+                } else {
+
+                }
+
+
+            }
+
+
+        }
+
+    }
+
+    public PrcessInfo parse(String str) {
+        String[] info = str.split(":");
+        int threadNo = Integer.parseInt(info[0]);
+        String process = info[1];
+        long time = Long.parseLong(info[2]);
+        ExclusiveTime.PrcessInfo processInfo = new PrcessInfo();
+        processInfo.setT(time);
+        processInfo.setProcessStatus(process);
+        processInfo.setTrdNo(threadNo);
+        return processInfo;
+    }
+
+    class PrcessInfo {
+        int trdNo;
+        String processStatus;
+        Long t;
+
+        public int getTrdNo() {
+            return trdNo;
+        }
+
+        public void setTrdNo(int trdNo) {
+            this.trdNo = trdNo;
+        }
+
+        public String getProcessStatus() {
+            return processStatus;
+        }
+
+        public void setProcessStatus(String processStatus) {
+            this.processStatus = processStatus;
+        }
+
+        public Long getT() {
+            return t;
+        }
+
+        public void setT(Long t) {
+            this.t = t;
+        }
+    }
+
+    public static void main(String... args) {
+        ExclusiveTime time = new ExclusiveTime();
+        List<String> list = new ArrayList<String>() {
+        };
+        list.add("0:start:0");
+        list.add("0:start:2");
+        list.add("0:end:5");
+        list.add("1:start:7");
+        list.add("1:end:7");
+        list.add("0:end:8");
+        int[] ints = time.exclusiveTime(2, list);
+        System.out.println(JSON.toJSON(ints));
+    }
+}
